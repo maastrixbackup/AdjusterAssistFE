6e9556e72026fe0bcee0ade3dee0b946cc86c547
Ritishree-12 <ritishree1996@gmail.com>
2026-03-02 19:05:29 +0530

    login screen, home screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,32 +2,59 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 
 import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: '#1469C9',
+        tabBarInactiveTintColor: '#8C97A8',
         headerShown: false,
         tabBarButton: HapticTab,
+        tabBarStyle: {
+          height: 86,
+          paddingTop: 8,
+          paddingBottom: 10,
+          borderTopColor: '#D0D7E2',
+          borderTopWidth: 1,
+          backgroundColor: '#F8FAFC',
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: '500',
+        },
       }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="home" color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'History',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons size={28} name="clock-outline" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="guides"
+        options={{
+          title: 'Guides',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons size={28} name="file-document-edit-outline" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="settings" color={color} />,
         }}
       />
     </Tabs>
