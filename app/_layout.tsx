ab3c75231613884b2f395b6ec3674e34820f55a3
Rudranarayan <rudranarayan.maastrix@gmail.com>
2026-06-04 10:50:10 +0530

    Initial commit sentry

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -16,9 +16,23 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "react-native-reanimated";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { toast, Toaster } from "sonner-native";
-import { ActivityIndicator, Platform, View } from "react-native";
+import { ActivityIndicator, View } from "react-native";
+import * as Sentry from '@sentry/react-native';
 
-
+Sentry.init({
+  dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
+  enabled: !__DEV__,
+  environment: process.env.EXPO_PUBLIC_APP_ENV ?? (__DEV__ ? "development" : "production"),
+  sendDefaultPii: false,
+  enableLogs: !__DEV__,
+  tracesSampleRate: __DEV__ ? 1.0 : 0.2,
+  replaysSessionSampleRate: 0.02,
+  replaysOnErrorSampleRate: 1.0,
+  integrations: [
+    Sentry.mobileReplayIntegration(),
+    Sentry.feedbackIntegration(),
+  ],
+});
 export const unstable_settings = {
   initialRouteName: "onboarding",
 };
@@ -115,7 +129,7 @@ function NavigationGuard() {
   );
 }
 
-export default function RootLayout() {
+export default Sentry.wrap(function RootLayout() {
   const colorScheme = useColorScheme();
   const [queryClient] = useState(() => new QueryClient());
 
@@ -141,7 +155,7 @@ export default function RootLayout() {
       </SafeAreaProvider>
     </GestureHandlerRootView>
   );
-}
+});
 
 // Defined inside layout, but BELOW the provider
 function AppContent() {
@@ -218,6 +232,14 @@ function AppContent() {
       subscription.remove();
     };
 
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      Sentry.captureException(
+        new Error("AdjusterAssist Sentry Test Error")
+      );
+    }, 3000);
   }, []);
 
   return (
